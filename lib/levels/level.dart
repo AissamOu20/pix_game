9bdb07e6cd893be0a2d3b4253c02a7bc9fa36ed8
Aissam Ou <aissamougunir2@gmail.com>
2023-12-11 13:13:57 +0100

    Initial commit

---
+++ lib/levels/level.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+
+class Level extends World {
+  @override
+  FutureOr<void> onLoad() {
+    // TODO: implement onLoad
+    return super.onLoad();
+  }
+}
